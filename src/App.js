02a49e14dc06c5a99920504dc01942940fc79029
Victor Vasquez <daxos10.0@gmail.com>
2022-03-30 23:36:23 -0500

    despliegue

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [responseData, setResponseData] = useState([]);
+  const [isSubmited, setIsSubmited] = useState(false);
+
+
+  useEffect( () => {
+    axios.get( 'https://pokeapi.co/api/v2/pokemon/?limit=807' )
+      .then( response => {
+        const listaPokemon = response.data.results.map( (pokemon, index) => {
+          return {
+            name : pokemon.name,            
+          }
+        });
+        setResponseData( (pokemonsPrev) => listaPokemon );  
+        console.log(listaPokemon);    
+      });
+  }, []);
+
+  return (    
+    <div className="App">      
+      <form onSubmit={ (e) => {setIsSubmited(true); e.preventDefault()}}>
+        <div>
+          <input type="submit" value="Fetch them all!" />      
+        </div>   
+      </form>
+
+      <ul>
+      { isSubmited &&  
+        responseData.map((pokemon, index) => {
+          return (
+          <li key={index}>
+            {pokemon.name}
+          </li>
+          )
+        })
+      }
+      </ul>      
     </div>
   );
-}
+  }
+
 
 export default App;
